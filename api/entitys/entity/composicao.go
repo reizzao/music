b7a645997f469c182df09cc7d506b5f7a23496a1
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-08-05 13:30:55 -0300

    refactor: organizacao entidades

--- api/entitys/entity/composicao.go
+++ api/entitys/entity/composicao.go
@@ -5,4 +5,9 @@ import "github.com/reizzao/music/api/entitys/entitySub"
 type Composicao struct {
 	Musica    entitySub.MusicaComposicao
 	Categoria entitySub.CategoriaMusical
+	Letra string
+	DataComposicao string // date
+	Tom string
+	Harmonia []entitySub.Harmonia
+
 }
---
+++ api/entitys/entitySub/harmonia.go
@@ -0,0 +1,6 @@
+package entitySub
+
+type Harmonia struct {
+	Parte   string // todo interface : PartesMusica
+	Acordes []string
+}
