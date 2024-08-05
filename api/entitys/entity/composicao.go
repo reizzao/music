7079079db471225352258d509c18f22a26484de3
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-08-05 13:34:13 -0300

    ok

--- api/entitys/entity/composicao.go
+++ api/entitys/entity/composicao.go
@@ -3,11 +3,10 @@ package entity
 import "github.com/reizzao/music/api/entitys/entitySub"
 
 type Composicao struct {
-	Musica    entitySub.MusicaComposicao
-	Categoria entitySub.CategoriaMusical
-	Letra string
-	DataComposicao string // date
-	Tom string
-	Harmonia []entitySub.Harmonia
-
+	Musica         entitySub.MusicaComposicao
+	Categoria      entitySub.CategoriaMusical
+	Letra          string
+	DataComposicao string // todo : date
+	Tom            string
+	Harmonia       []entitySub.Harmonia
 }
