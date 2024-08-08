72008387ae2bd03a7f12179d1f185bd20b7a5df8
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-08-08 14:33:10 -0300

    feat: ok - parte musica

--- api/modules/voz/seed/seedvoz/parte.musica.seed.go
+++
@@ -1,8 +0,0 @@
-package seedvoz
-
-import "github.com/reizzao/music/api/modules/voz/contractvoz"
-
-var Parte_A = contractvoz.ParteMusica{
-	Parte: "A",
-	Clima: []contractvoz.ClimaParte{},
-}
---
+++ api/modules/voz/seed/seedvoz/partes.musica.seed.go
@@ -0,0 +1,26 @@
+package seedvoz
+
+import (
+	"github.com/reizzao/music/api/modules/voz/contractvoz"
+)
+
+var Parte_A_Baixo_Piano = contractvoz.ClimaParte{
+	Nome: "Parte_A_Baixo_Piano",
+	GrausCombina: []contractvoz.Grau{
+		Grau_1,
+	},
+}
+
+var Parte_B_Medio_Crescendo = contractvoz.ClimaParte{
+	Nome: "Parte_B_Medio_Crescendo",
+	GrausCombina: []contractvoz.Grau{
+		Grau_4, Grau_6,
+	},
+}
+
+var Parte_REFRAO_Forte = contractvoz.ClimaParte{
+	Nome: "Parte_REFRAO_Forte",
+	GrausCombina: []contractvoz.Grau{
+		Grau_5, Grau_7,
+	},
+}
